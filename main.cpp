bdfe5620f46fece6225a43a26b0a54559d8f8e32
Hugo Latendresse <hugolatendresse@gmail.com>
2024-05-20 12:08:15 -0700

    chunk struct

--- main.cpp
+++ main.cpp
@@ -7,32 +7,31 @@
 #include <windows.h>
 #include <iostream>
 
-bool is_png(const char *path) {
-    FILE *file = std::fopen(path, "rb");
-    if (file == nullptr) {
-        std::perror("Could not open file");
-        return false;
-    }
+#define SIGNATURE_LENGTH 8
+
+bool is_png(unsigned char *signature) {
     unsigned char expected_signature[] = {137, 80, 78, 71, 13, 10, 26, 10};
 
-    size_t signature_len = sizeof(expected_signature) / sizeof(expected_signature[0]);
-
-    unsigned char buffer[signature_len];
-    std::fread(buffer, 1, 8, file);
-
-    if (!std::equal(buffer, buffer + signature_len, expected_signature)){
+    if (!std::equal(signature, signature + SIGNATURE_LENGTH, expected_signature)){
         perror("This is not a PNG file!");
         return false;
     }
 
-//    for (int i = 0; i < 8; i++) {
-//        std::printf("%d ", buffer[i]);
-//    }
-
-    std::fclose(file);
     return true;
 
 }
+
+
+typedef struct ChunkHeader_ {
+    unsigned int length;
+    char type[5];
+} ChunkHeader;
+
+typedef struct Chunk_ {
+    ChunkHeader *header;
+    unsigned char *data;
+    unsigned int crc;
+} Chunk;
 
 
 
@@ -45,6 +44,32 @@ int main(int argc, char *argv[]) {
         path = "icon.png";
     }
 
-    std::cout << "It is a png: " << (is_png(path));
+    // TODO opening the file twice is inefficient. Move all to a class and do evertyhing at once?
+
+    FILE *file = std::fopen(path, "rb");
+    if (file == nullptr) {
+        std::perror("Could not open file");
+        return false;
+    }
+
+    // Read signature
+    unsigned char signature[SIGNATURE_LENGTH];
+    std::fread(signature, 1, SIGNATURE_LENGTH, file);
+    std::cout << "It is a png: " << (is_png(signature)) << std::endl;
+
+
+
+
+    ChunkHeader *chunk = static_cast<ChunkHeader *>(malloc(sizeof(ChunkHeader)));
+
+    std::fread(chunk, sizeof(ChunkHeader), 1, file);
+    chunk->type[4] = '\0'; // null-terminate the string
+
+    std::cout << "Chunk length: " << chunk->length << std::endl; // type-safe AND can work with
+    // user_defined types
+    std::cout << "Chunk type: " << chunk->type << std::endl;
+
+    free(chunk);
+
     return 0;
 }
