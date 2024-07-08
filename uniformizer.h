a3cd4766aba0d7075e027665de4daaefeb577e66
Hugo Latendresse <hugolatendresse@gmail.com>
2024-07-08 14:59:30 -0700

    struct RBGA_Pixel

--- uniformizer.h
+++ uniformizer.h
@@ -2,7 +2,15 @@
 extern "C" {
 #endif
 
+    typedef struct rgba_pixel_ {
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+        unsigned char a;
+    } RGBA_Pixel;
+
     int display_image(int height, int width, unsigned char *data);
+    int transform(png_bytep buffer, png_uint_32 height, png_uint_32 width, png_int_32 row_stride, int format);
 
 #ifdef __cplusplus
 }
