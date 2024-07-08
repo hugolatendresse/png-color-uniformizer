52949321ac24ba3457c6815068a0c6f3c655e46f
Hugo Latendresse <hugolatendresse@gmail.com>
2024-07-08 14:39:33 -0700

    clean up manual read

--- manual.cpp
+++ manual.cpp
@@ -15,7 +15,6 @@
 #define SIGNATURE_LENGTH 8
 
 /*Globals*/
-// TODO refactor as attributes in OOP
 char color_type = -1;
 char compression_method = -1;
 char filtering_method = -1;
@@ -24,7 +23,7 @@ std::vector<unsigned char> compressed_idat;
 
 bool is_png(unsigned char *signature) {
     unsigned char expected_signature[] = {137, 80, 78, 71, 13, 10, 26, 10};
-    if (!std::equal(signature, signature + SIGNATURE_LENGTH, expected_signature)){
+    if (!std::equal(signature, signature + SIGNATURE_LENGTH, expected_signature)) {
         perror("This is not a PNG file!");
         return false;
     }
@@ -33,16 +32,8 @@ bool is_png(unsigned char *signature) {
 
 typedef struct ChunkHeader_ {
     unsigned int data_size;
-    char type[4 + 1];  // 4 character type
+    char type[4 + 1]; // 4 character type
 } ChunkHeader;
-
-
-// TODO can probably delete
-typedef struct ChunkStandard_ {
-    ChunkHeader *header;
-    char *data_content;
-    unsigned int crc;
-} ChunkStandard;
 
 typedef struct DataIHDR_ {
     int width;
@@ -54,8 +45,6 @@ typedef struct DataIHDR_ {
     char interlace_method;
 } DataIHDR;
 
-
-
 void switch_endianness(char *src, char *dest, int byte_cnt) {
     assert(src != dest);
     for (size_t i = 0; i < byte_cnt; i++) {
@@ -63,32 +52,29 @@ void switch_endianness(char *src, char *dest, int byte_cnt) {
     }
 }
 
-// Todo make this a method in a class
 void switch_endianness(char *p, int byte_cnt) {
     char buffer[byte_cnt];
     switch_endianness(p, buffer, byte_cnt);
     memcpy(p, buffer, byte_cnt);
 }
 
-// TODO refactor as OOP!
-
 void print_rgb_info() {
     std::cout << "Pixel is represented by three samples: red (zero = black, max = red) "
-                 "appears first, then green (zero = black, max = green), then blue "
-                 "(zero = black, max = blue). The bit depth specifies the size of each "
-                 "sample, not the total pixel size" << std::endl;
+            "appears first, then green (zero = black, max = green), then blue "
+            "(zero = black, max = blue). The bit depth specifies the size of each "
+            "sample, not the total pixel size" << std::endl;
 }
 
 void print_alpha_info() {
     std::cout << "An alpha value of zero represents full transparency, and a value of "
-                 "2^bitdepth represents a fully opaque pixel. " << std::endl;
+            "2^bitdepth represents a fully opaque pixel. " << std::endl;
 }
 
 void set_color_type(char color_type_input) {
     color_type = color_type_input;
     std::cout << std::endl << "Color type " << static_cast<int>(color_type) << " info:" <<
-    std::endl;
-    switch(color_type) {
+            std::endl;
+    switch (color_type) {
         case 0:
             std::cout << "Each pixel is a grayscale sample." << std::endl;
             break;
@@ -114,11 +100,11 @@ void set_color_type(char color_type_input) {
 void set_compression_method(char compression_input) {
     compression_method = compression_input;
     std::cout << std::endl << "Compression type " << static_cast<int>(compression_method)
-    << " info:" << std::endl;
-    switch(compression_method) {
+            << " info:" << std::endl;
+    switch (compression_method) {
         case 0:
             std::cout << "Deflate/inflate compression with a sliding window of at most 32768 "
-                         "bytes" << std::endl;
+                    "bytes" << std::endl;
             break;
         default:
             std::cerr << "Unknown compression type. PNG supports only method 0." << std::endl;
@@ -126,15 +112,16 @@ void set_compression_method(char compression_input) {
             break;
     }
 }
+
 void set_filtering_method(char filtering_input) {
     filtering_method = filtering_input;
     std::cout << std::endl << "Filtering type " << static_cast<int>(filtering_method) << " info:"
-    << std::endl;
-    switch(filtering_method) {
+            << std::endl;
+    switch (filtering_method) {
         case 0:
             std::cout << "None() filter, the scanline is transmitted unmodified;\n"
-                         "It is necessary only to insert a filter-type byte before the data." <<
-                         std::endl;
+                    "It is necessary only to insert a filter-type byte before the data." <<
+                    std::endl;
             break;
         default:
             std::cerr << "Filtering method not yet supported by this tool" << std::endl;
@@ -145,12 +132,12 @@ void set_filtering_method(char filtering_input) {
 void set_interlacing_method(char interlacing_input) {
     interlacing_method = interlacing_input;
     std::cout << std::endl << "Interlacing type " << static_cast<int>(interlacing_method)
-    << " info:" << std::endl;
-    switch(interlacing_method) {
+            << " info:" << std::endl;
+    switch (interlacing_method) {
         case 0:
             std::cout << "With interlace method 0, pixels are stored sequentially from left to "
-                         "right, and scanlines sequentially from top to bottom (no interlacing)." <<
-                         std::endl;
+                    "right, and scanlines sequentially from top to bottom (no interlacing)." <<
+                    std::endl;
             break;
         default:
             std::cerr << "Interlacing method not yet supported by this tool" << std::endl;
@@ -160,14 +147,13 @@ void set_interlacing_method(char interlacing_input) {
 
 // Passing compressed_data by reference. Avoids having to copy. Still can't modify the data since
 // it's const
-std::vector<unsigned char> decompress_idat(const std::vector<unsigned char>& compressed) {
+std::vector<unsigned char> decompress_idat(const std::vector<unsigned char> &compressed, DataIHDR *ihdr) {
     z_stream strm = {};
     strm.total_in = strm.avail_in = compressed.size();
-    strm.next_in = (Bytef*)compressed.data();
+    strm.next_in = (Bytef *) compressed.data();
 
     // Initialize the output buffer with a guess of the size of the decompressed data
-    std::vector<unsigned char> decompressed(128 * (128 * 4 + 1)); // Initial buffer size for 128x128 RGBA image
-    // TODO parametrize the above
+    std::vector<unsigned char> decompressed(ihdr->height * (ihdr->width * 4 + 1)); // Assumes RGBA image
 
     // Initialize the zlib decompression stream
     inflateInit(&strm);
@@ -176,17 +162,17 @@ std::vector<unsigned char> decompress_idat(const std::vector<unsigned char>& com
     int ret;
     do {
         strm.avail_out = decompressed.size() - strm.total_out;
-        strm.next_out = (Bytef*)(decompressed.data() + strm.total_out);
+        strm.next_out = (Bytef *) (decompressed.data() + strm.total_out);
 
         ret = inflate(&strm, Z_NO_FLUSH);
-        assert(ret != Z_STREAM_ERROR);  // State not clobbered
+        assert(ret != Z_STREAM_ERROR); // State not clobbered
 
         switch (ret) {
             case Z_NEED_DICT:
-                ret = Z_DATA_ERROR;     // And fall through
+                ret = Z_DATA_ERROR; // And fall through
             case Z_DATA_ERROR:
             case Z_MEM_ERROR:
-                (void)inflateEnd(&strm);
+                (void) inflateEnd(&strm);
                 throw std::runtime_error("Zlib decompression error");
         }
 
@@ -202,7 +188,6 @@ std::vector<unsigned char> decompress_idat(const std::vector<unsigned char>& com
 
     return decompressed;
 }
-
 
 
 int main(int argc, char *argv[]) {
@@ -224,7 +209,7 @@ int main(int argc, char *argv[]) {
     std::fread(signature, 1, SIGNATURE_LENGTH, file);
     std::cout << "It is a png: " << (is_png(signature)) << std::endl;
 
-    DataIHDR *data_ihdr = static_cast<DataIHDR *>(malloc(sizeof(DataIHDR)));
+    DataIHDR *ihdr = static_cast<DataIHDR *>(malloc(sizeof(DataIHDR)));
 
     while (!std::feof(file)) {
         // Grab header of chunk
@@ -233,13 +218,12 @@ int main(int argc, char *argv[]) {
         std::fread(&(chunk_header->type), sizeof(chunk_header->type) - 1, 1, file);
         chunk_header->type[4] = '\0';
         switch_endianness((char *) (&(chunk_header->data_size)), (int) sizeof
-                (chunk_header->data_size));
+                          (chunk_header->data_size));
 
         std::cout << std::endl << "Chunk type: " << chunk_header->type << std::endl;
         std::cout << "Chunk data_size: " << chunk_header->data_size << std::endl;
 
         // Grab data
-        // TODO refactor into a select statement
         if (strcmp(chunk_header->type, "IHDR") == 0) {
             /*
             The IHDR chunk must appear FIRST. It contains:
@@ -252,46 +236,40 @@ int main(int argc, char *argv[]) {
             Filter method:      1 byte
             Interlace method:   1 byte
             */
-            std::fread(&(data_ihdr->width), sizeof(data_ihdr->width), 1, file);
-            switch_endianness((char *)&(data_ihdr->width), sizeof(data_ihdr->width));
-            std::fread(&(data_ihdr->height), sizeof(data_ihdr->height), 1, file);
-            switch_endianness((char *) &(data_ihdr->height), sizeof(data_ihdr->height));
-            std::fread(&(data_ihdr->bit_depth), sizeof(data_ihdr->bit_depth), 1, file);
-            std::fread(&(data_ihdr->color_type), sizeof(data_ihdr->color_type), 1, file);
-            std::fread(&(data_ihdr->compress_method), sizeof(data_ihdr->compress_method), 1, file);
-            std::fread(&(data_ihdr->filter_method), sizeof(data_ihdr->filter_method), 1, file);
-            std::fread(&(data_ihdr->interlace_method), sizeof(data_ihdr->interlace_method), 1, file);
-            std::cout << "width: " << data_ihdr->width << std::endl;
-            std::cout << "height: " << data_ihdr->height << std::endl;
-            std::cout << "bit_depth: " << (int) (data_ihdr->bit_depth) << std::endl;
-            std::cout << "color_type: " << (int) (data_ihdr->color_type) << std::endl;
-            std::cout << "compress_method: " << (int) (data_ihdr->compress_method) << std::endl;
-            std::cout << "filter_method: " << (int) (data_ihdr->filter_method) << std::endl;
-            std::cout << "interlace_method: " << (int) (data_ihdr->interlace_method) << std::endl;
+            std::fread(&(ihdr->width), sizeof(ihdr->width), 1, file);
+            switch_endianness((char *) &(ihdr->width), sizeof(ihdr->width));
+            std::fread(&(ihdr->height), sizeof(ihdr->height), 1, file);
+            switch_endianness((char *) &(ihdr->height), sizeof(ihdr->height));
+            std::fread(&(ihdr->bit_depth), sizeof(ihdr->bit_depth), 1, file);
+            std::fread(&(ihdr->color_type), sizeof(ihdr->color_type), 1, file);
+            std::fread(&(ihdr->compress_method), sizeof(ihdr->compress_method), 1, file);
+            std::fread(&(ihdr->filter_method), sizeof(ihdr->filter_method), 1, file);
+            std::fread(&(ihdr->interlace_method), sizeof(ihdr->interlace_method), 1, file);
+            std::cout << "width: " << ihdr->width << std::endl;
+            std::cout << "height: " << ihdr->height << std::endl;
+            std::cout << "bit_depth: " << (int) (ihdr->bit_depth) << std::endl;
+            std::cout << "color_type: " << (int) (ihdr->color_type) << std::endl;
+            std::cout << "compress_method: " << (int) (ihdr->compress_method) << std::endl;
+            std::cout << "filter_method: " << (int) (ihdr->filter_method) << std::endl;
+            std::cout << "interlace_method: " << (int) (ihdr->interlace_method) << std::endl;
 
-            set_color_type(data_ihdr->color_type);
-            set_compression_method(data_ihdr->compress_method);
-            set_filtering_method(data_ihdr->filter_method);
-            set_interlacing_method(data_ihdr->interlace_method);
+            set_color_type(ihdr->color_type);
+            set_compression_method(ihdr->compress_method);
+            set_filtering_method(ihdr->filter_method);
+            set_interlacing_method(ihdr->interlace_method);
 
             // In my case, I have color_type == 6, which means Each pixel is an R,G,B triple, followed by
             // an alpha sample.
             // TODO generalize that for other possible color types
-            assert(data_ihdr->color_type == 6);  // TODO remove for generic tool
+            assert(ihdr->color_type == 6);
 
             // Only compression method currently defined for png is 0 (deflate/inflat compression)
-            assert(data_ihdr->compress_method == 0);
+            assert(ihdr->compress_method == 0);
 
             // Only filter method currently defined for png is 0 (adaptive filtering with five basic filter types)
-            assert(data_ihdr->filter_method == 0);
-
+            assert(ihdr->filter_method == 0);
         } else if (strcmp(chunk_header->type, "PLTE") == 0) {
             std::cout << "reached PLTE" << std::endl;
-            // TODO
-            //        chunk->standard.data_content =static_cast<char *>(calloc(1, chunk_header->data_size));
-            //        for(size_t i = 0; i < chunk_header->data_size; i++) {
-            //            std:fread(chunk->data_content + i, 1, 1, file);
-            //        }
         } else if (strcmp(chunk_header->type, "IDAT") == 0) {
             /*
             The IDAT chunk contains the actual image data. To create this data:
@@ -310,12 +288,12 @@ int main(int argc, char *argv[]) {
             */
 
             std::cout << "reached IDAT" << std::endl;
-            char *data_content =static_cast<char *>(calloc(1, chunk_header->data_size));
-            for(size_t i = 0; i < chunk_header->data_size; i++) {
+            char *data_content = static_cast<char *>(calloc(1, chunk_header->data_size));
+            for (size_t i = 0; i < chunk_header->data_size; i++) {
                 std::fread(data_content + i, 1, 1, file);
             }
             compressed_idat.insert(compressed_idat.end(), data_content,
-                                     data_content + chunk_header->data_size);
+                                   data_content + chunk_header->data_size);
         } else if (strcmp(chunk_header->type, "sRGB") == 0) {
             /*
             The sRGB chunk contains:
@@ -330,9 +308,8 @@ int main(int argc, char *argv[]) {
             */
             char rendering_intent[1];
             std::fread(rendering_intent, 1, 1, file);
-            // TODO for my benefit, compare different factorization of ElementSize * Count in fread
             std::cout << "Rendering Intent: ";
-            switch(rendering_intent[0]) {
+            switch (rendering_intent[0]) {
                 case 0:
                     /*
                     Perceptual intent is for images preferring good adaptation to the output device
@@ -366,12 +343,8 @@ int main(int argc, char *argv[]) {
             std::cout << std::endl;
         } else if (strcmp(chunk_header->type, "gAMA") == 0) {
             std::cout << "reached gAMA" << std::endl;
-            // TODO
-            //        }
         } else if (strcmp(chunk_header->type, "gAMA") == 0) {
             std::cout << "reached gAMA" << std::endl;
-            // TODO
-            //        }
         } else if (strcmp(chunk_header->type, "IEND") == 0) {
             std::cout << "reached IEND" << std::endl;
             fclose(file);
@@ -390,38 +363,29 @@ int main(int argc, char *argv[]) {
         switch_endianness(crc, sizeof(crc));
 
         free(chunk_header);
-
     }
-    // TODO fix having both decompressed_idat and uncompressed_idat
     std::cout << "Now need to unpack the IDAT data" << std::endl;
 
     std::vector<unsigned char> decompressed_idat;
     try {
-        decompressed_idat = decompress_idat(compressed_idat);
+        decompressed_idat = decompress_idat(compressed_idat, ihdr);
         std::cout << "Decompressed successfully. Size: " << decompressed_idat.size() << " bytes" << std::endl;
-    } catch (const std::runtime_error& e) {
+    } catch (const std::runtime_error &e) {
         std::cerr << "Decompression failed: " << e.what() << std::endl;
     }
 
     // Ignore the filtering bytes - but do it pixel by pixel to be able to change pixels
     std::vector<unsigned char> d4;
-    for (std::vector<unsigned char>::size_type row=0; row<128; row++) {
-        // TODO parametrize all 128 and stuff
-        // TODO col=4 seems to be giving the best results, meaning that I propably still don't understand someting re. endianness
-        for (std::vector<unsigned char>::size_type col=1; col<(128*4+1); col+= 4) { // TODO parametrize all 128 and stuff
-            // d4.insert(d4.end(),
-            //     decompressed_idat.data() + row*(128*4+1) + col,
-            //     decompressed_idat.data() + row*(128*4+1) +col + 4);
-            //
-            std::vector<unsigned char> pixel(decompressed_idat.data() + row * (128 * 4 + 1) + col,
-                                             decompressed_idat.data() + row * (128 * 4 + 1) + col + 4);
+    for (std::vector<unsigned char>::size_type row = 0; row < ihdr->height; row++) {
+        for (std::vector<unsigned char>::size_type col = 1; col < (ihdr->width * 4 + 1); col += 4) {
+            int stride = ihdr->width * 4 + 1;
+            std::vector<unsigned char> pixel(decompressed_idat.data() + row * stride + col,
+                                             decompressed_idat.data() + row * stride + col + 4);
             // R G B Alpha
             pixel[0] = (pixel[0] + 0) % 256;
             pixel[1] = (pixel[1] + 0) % 256;
             pixel[2] = (pixel[2] + 0) % 256;
             pixel[3] = (pixel[3] + 0) % 256;
-
-            // TODO understand why the result when running is different from the original pic! Is decompression wrong?
 
             // set all to black
             // pixel[0] = 0;
@@ -436,7 +400,6 @@ int main(int argc, char *argv[]) {
             // unsigned char temp2 = pixel[1];
             // pixel[1] = pixel[2];
             // pixel[2] = temp2;
-
 
 
             // This is used to accentuate all colors
@@ -467,7 +430,6 @@ int main(int argc, char *argv[]) {
             // }
 
 
-
             // Invert everything
             // pixel[0] = UCHAR_MAX - pixel[0];
             // pixel[1] = UCHAR_MAX - pixel[1];
@@ -479,13 +441,9 @@ int main(int argc, char *argv[]) {
     }
 
     std::cout << "stop here";
-    int width = 128;
-    int height = 128;
 
-    display_image(data_ihdr->height, data_ihdr->width, d4.data());
+    display_image(ihdr->height, ihdr->width, d4.data());
 
-    free(data_ihdr);
+    free(ihdr);
     return 0;
-
-    // TODO: assert the following: IHDR must appear first and IEND must appear last;
 }
